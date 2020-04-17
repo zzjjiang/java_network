ac11ffab296b23937bddf6db1847befbe6c9adb0
zzjjiang <zoujone@163.com>
2020-04-17 16:27:08 +0800

    1.项目搭建 2.bio提交

---
+++ src/main/java/com/demo/bio/BioServer.java
@@ -0,0 +1,39 @@
+package com.demo.bio;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @description bio通信服务端  该方式只能监听一次
+ * @author zzj
+ * @date 2020.04.14
+ */
+public class BioServer {
+    public static void main(String[] args) throws Exception {
+        //服务器必备
+        ServerSocket serverSocket = new ServerSocket();
+        //绑定监听端口
+        SocketAddress socketAddress = new InetSocketAddress(10001);
+        serverSocket.bind(socketAddress);
+        System.out.println("Server start.......");
+        Socket socket = serverSocket.accept();
+
+        ObjectInputStream inputStream
+                = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream outputStream
+                = new ObjectOutputStream(socket.getOutputStream());
+        /*服务器的输入*/
+        String userName = inputStream.readUTF();
+        System.out.println("服务器接收到的客户端的信息为:"+userName);
+
+        outputStream.writeUTF("Hello,"+userName);
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+        socket.close();
+    }
+}
---
+++ src/main/java/com/demo/bio/BioServerUpgrade.java
@@ -0,0 +1,67 @@
+package com.demo.bio;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @description 服务端多线程监听
+ * @author zzj
+ * @date 2020.04.14
+ */
+public class BioServerUpgrade {
+
+    public static void main(String[] args) throws IOException {
+        //服务器必备
+        ServerSocket serverSocket = new ServerSocket();
+        //设置端口
+        SocketAddress socketAddress = new InetSocketAddress(10002);
+        //绑定端口
+        serverSocket.bind(socketAddress);
+        //接受消息,通过socket通信
+        System.out.println("Server start.......");
+        while (true){
+            new Thread(new ServerTask(serverSocket.accept())).start();
+        }
+    }
+
+    private static class ServerTask implements Runnable{
+
+        private Socket socket;
+        public ServerTask(Socket socket){
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            /*拿和客户端通讯的输入输出流*/
+            try(ObjectInputStream inputStream
+                        = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream outputStream
+                        = new ObjectOutputStream(socket.getOutputStream())){
+
+                /*服务器的输入*/
+                String userName = inputStream.readUTF();
+                System.out.println("Accept clinet message:"+userName);
+
+                outputStream.writeUTF("Hello,"+userName);
+                outputStream.flush();
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
